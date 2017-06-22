17fdd9b270eac3aaf7f7fb88da58ce6f3188aae5
Daniel-Ems <danielems3@outlook.com>
2017-06-22 19:35:52 -0400

    concatenate directories

--- ashti.c
+++ ashti.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+#include <limits.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[])
 {
@@ -12,12 +17,19 @@ int main(int argc, char *argv[])
     }
     
     //strip out the directory
-    char * directory = argv[1];
+    char *directory = "\0";
+    directory = argv[1];
  
     //TEST
     printf("directory entered %s\n", directory);
 
+   
+    char *path = calloc(PATH_MAX, sizeof(char) );
+    strncat(path, "~/", 3);
+    strncat(path, directory, strlen(directory));
 
+    printf("dir : %s\n", path);
+    free(path);
     return 0;
 
 }
